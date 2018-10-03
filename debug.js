99e8ca6ed8bd09fada88d52ba7cc94f4abd3ecf8
Sridharan Murugadass <sridharanbe.ece@gmail.com>
2018-10-03 15:00:06 +0530

    Update debug.js

--- debug.js
+++ debug.js
@@ -6,11 +6,9 @@
 
 const options = {};
 
-// options.ip = '127.0.0.1';
-// options.port = parseInt(process.argv[2]);
-// options.config = { name: 'Total.js' };
-// options.sleep = 3000;
-// options.inspector = 9229;
-// options.watch = ['private'];
+ options.ip = '127.0.0.1';
+ options.port = parseInt(process.argv[2]);
+ options.config = { name: 'Total.js' };
+ options.sleep = 3000;
 
-require('total.js/debug')(options);
+require('total.js').http('release', options);
